0cd790b12b643bca749ec87b3177198ab6d7cc4c
Leonard <leonardwkee@gmail.com>
2021-12-08 23:53:31 -0800

    part 2.2

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import HomeScreen from "./src/screens/HomeScreen";
+import { createAppContainer } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
+import HomeScreen from "./src/screens/HomeScreen"
+import ComponentsScreen from './src/screens/ComponentsScreen'
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
   },
   {
-    initialRouteName: "Home",
+    Components: ComponentsScreen,
+  },
+  {
+    initialRouteName: "Components",
     defaultNavigationOptions: {
       title: "App",
     },
   }
-);
+)
 
-export default createAppContainer(navigator);
+export default createAppContainer(navigator)

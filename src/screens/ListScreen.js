121d08e7b3d58dbab97249705cece2b277a34c4b
Leonard <leonardwkee@gmail.com>
2021-12-10 21:10:28 -0800

    part 4.2: add button to home page

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Stylesheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
   const friends = [

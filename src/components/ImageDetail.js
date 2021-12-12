e6277e37cf39782d4eff7ec43edbcf840cb14402
Leonard <leonardwkee@gmail.com>
2021-12-11 18:39:01 -0800

    part 5.3: add ImageDetail component

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+const ImageDetail = () => {
+  return (
+    <Text>Image Detail</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+})
+
+export default ImageDetail

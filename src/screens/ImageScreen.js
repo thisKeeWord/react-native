2d4dfa8b0cd9e04ba0c8f626dc0e582af9e0cb8e
Leonard <leonardwkee@gmail.com>
2021-12-11 21:58:45 -0800

    part 6.9 - 6.18: add color adjuster via state, then redo using reducer"

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import ImageDetail from '../components/ImageDetail'
 
-
-
 const ImageScreen = () => {
   return (
     <View>

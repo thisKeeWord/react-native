aac93695cf34e413fe075157434e1ec109a211bd
Leonard <leonardwkee@gmail.com>
2021-12-11 19:38:14 -0800

    part 5.7: add score prop to ImageDetail

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -7,9 +7,21 @@ import ImageDetail from '../components/ImageDetail'
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} />
-      <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} />
-      <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} />
+      <ImageDetail
+        title="Forest"
+        imageSource={require('../../assets/forest.jpg')}
+        score={9}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require('../../assets/beach.jpg')}
+        score={7}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require('../../assets/mountain.jpg')}
+        score={4}
+      />
     </View>
   )
 }

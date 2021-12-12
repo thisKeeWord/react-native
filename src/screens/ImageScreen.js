b0ba3c0d8c10037fc292d84488da6b351d8dd44c
Leonard <leonardwkee@gmail.com>
2021-12-11 19:32:01 -0800

    part 5.6: add image prop

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import ImageDetail from '../components/ImageDetail'
 
+
+
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail title="Forest" />
-      <ImageDetail title="Beach" />
-      <ImageDetail title="Mountain" />
+      <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} />
+      <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} />
+      <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} />
     </View>
   )
 }
